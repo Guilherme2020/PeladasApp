bd48b1713b32c889c39e05c52bf2a5e38ecaf9cc
guilherme2020 <guilhermerodrigues123477@gmail.com>
2018-08-18 14:57:09 -0300

    uso de vuex para guardar token,username e usermail  e transicao de tela

--- client/src/components/home/Index.vue
+++ client/src/components/home/Index.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
     <h1>Peladas Login</h1>
-    <h2>Em  breve Loginn</h2>
-    h3
+
+
+    <h3>Seja Bem vindo  {{user}}</h3>
+    <h3>email: {{userEmail}}</h3>
   </div>
 </template>
+
+<script>
+    export  default {
+
+      computed:{
+
+        user(){
+        	const { name} = this.$store.state.user;
+
+        	return	`o Usuario logado é ${name}`
+        },
+        userEmail(){
+          const {email} = this.$store.state.user;
+          return `O emai é ${email}`
+        },
+        userName(){
+            return this.$store.state.user.name
+
+        },
+
+      }
+
+
+
+    }
+
+
+</script>
--- client/src/store.js
+++ client/src/store.js
@@ -3,16 +3,18 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios'
 import Token from './services/token'
-
+import SessionStorage from './services/session-storage'
 import router from "./router/index";
 
 Vue.use(Vuex);
 
-const state = {
+const  userName =  window.sessionStorage.getItem("username");
+const userEmail = window.sessionStorage.getItem("email");
 
+const state = {
   user:{
-    name: "",
-    email: "",
+    name: userName,
+    email: userEmail
   },
 };
 
